2f0f91557b2a7fc098c2dcb624da9b1367057403
danyoh <danyoh#gmail.com>
2025-02-22 20:38:32 +0900

    테이블 툴바에 필터 처리 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        {/* SHADCN Toaster 사용함 */}
+        <Toaster />
       </body>
     </html>
   );
